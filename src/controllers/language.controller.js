7157416eec7b8eb2340e405eb731cd0edc187bd1
Moises <egcgion15@gmail.com>
2022-07-10 13:47:00 -0500

    Act

--- src/controllers/language.controller.js
+++ src/controllers/language.controller.js
@@ -8,7 +8,7 @@ const getLanguage= async(request, response)=>{
             "nombre":result,
             "msm": "bien"
         }
-        response.json(JSON.stringify(result))
+        response.json(result)
         
     } catch (error) {
         response.status(500);
